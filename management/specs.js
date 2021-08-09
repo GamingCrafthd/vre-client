dd39797f29e0763693d0a2e2d4637d314b2c61dd
JustMattis <60194722+JustMattis@users.noreply.github.com>
2021-08-09 23:02:55 +0200

    spec overhaul

---
+++ management/specs.js
@@ -0,0 +1,68 @@
+var specs = []
+
+function updateCurrentVehicle(current_vehicle) {
+    specs = httpGet(`http://${localStorage.getItem("ipv4")}/api/vehicles/${current_vehicle}/specs/${localStorage.getItem("sessionId")}`).res.split(";")
+    let index = 0
+    specs.forEach(spec => {
+        specs[index] = { key: spec.split("=")[0].replace(/%2F/g, "/"), value: spec.split("=")[1].replace(/%2F/g, "/") }
+        index++
+    })
+    update()
+}
+
+const specs_div = () => document.getElementById('specs')
+
+function addSpec() {
+    specs.push({ key: "", value: "" })
+    update()
+}
+
+function removeSpec(index) {
+    specs = specs.splice(index, 1)
+    update()
+}
+
+function getSpecsFormatted() {
+    var specs_formatted = []
+    specs.forEach(spec => specs_formatted.push(`${spec.key.replace(/\//g, "%2F")}=${spec.value.replace(/\//g, "%2F")}`))
+    return specs_formatted.join(";").replace(/;=/g, "")
+}
+
+function update() {
+    specs_div().innerHTML = ''
+
+    let index = 0
+    specs.forEach(spec => {
+
+        let specDiv = document.createElement("div")
+        specDiv.id = index
+
+        let key = document.createElement("input")
+        let value = document.createElement("input")
+
+        key.id = "key_" + index
+        value.id = "value_" + index
+
+        key.addEventListener('change', () => {
+            console.log(key.value)
+            specs[parseInt(key.id.replace("key_", ""))].key = key.value
+        })
+
+        value.addEventListener('change', () => {
+            console.log(value.value)
+            specs[parseInt(value.id.replace("key_", ""))].value = value.value
+        })
+
+        specDiv.appendChild(key)
+        specDiv.appendChild(value)
+        specs_div().appendChild(specDiv)
+        index++
+    })
+
+    index = 0
+    specs.forEach(spec => {
+        document.getElementById("key_" + index).value = spec.key
+        document.getElementById("value_" + index).value = spec.value
+        index++
+    })
+}
--- management/vehicles.js
+++ management/vehicles.js
@@ -1,7 +1,7 @@
 const select = document.getElementById("select")
 const vehicles = JSON.parse(httpGet(`http://${localStorage.getItem("ipv4")}/api/vehicles/${localStorage.getItem("sessionId")}`).res)
 const specTemplate = '<div id="%id"><input id="key_%id"><input id="value_%id"><button class="btn btn-danger" id="bn_delete_%id">Löschen</button><br></div>'
-const newSpecTemplate = '<button type="button" class="btn btn-primary" id="bn_edit_new_spec_%vehicle" onclick="%onclick">Neue Spezifikation hinzufügen</button>'
+const newSpecTemplate = '<button type="button" class="btn btn-primary" id="bn_edit_new_spec_%vehicle" onclick="addSpec()">Neue Spezifikation hinzufügen</button>'
 
 document.remove = x => { document.getElementById(x).parentNode.removeChild(document.getElementById(x)) }
 
@@ -29,19 +29,12 @@ function deleteVehicle(vehicle) {
 }
 
 function openEdit(vehicle) {
+    updateCurrentVehicle(vehicle)
+
     document.getElementById("start").hidden = true
     document.getElementById("edit").hidden = false
 
     document.getElementById("edit_name").innerText = vehicle
-    document.getElementById("edit_specs").innerHTML = ""
-
-    const specs = httpGet(`http://${localStorage.getItem("ipv4")}/api/vehicles/${vehicle}/specs/${localStorage.getItem("sessionId")}`).res.split(";")
-    specs.forEach(() => {
-        const id = getIndex()
-        document.getElementById("edit_specs").innerHTML += specTemplate.replace(/%id/g, id)
-        document.getElementById(`bn_delte_${id}`).addEventListener("click", () => deleteSpec(id))
-    })
-    updateSpecs(vehicle)
 
     const manufacturer = httpGet(`http://${localStorage.getItem("ipv4")}/api/vehicles/${vehicle}/manufacturer/${localStorage.getItem("sessionId")}`).res
     const type = httpGet(`http://${localStorage.getItem("ipv4")}/api/vehicles/${vehicle}/type/${localStorage.getItem("sessionId")}`).res
@@ -49,50 +42,16 @@ function openEdit(vehicle) {
     document.getElementById("manufacturer").value = manufacturer
     document.getElementById("type").value = type
     document.getElementById('edit_bn').innerHTML = ""
-    document.getElementById('edit_bn').innerHTML += newSpecTemplate.replace('%vehicle', vehicle).replace("%onclick", `addSpec('${vehicle}')`)
-    document.getElementById('edit_bn').innerHTML += '<br><br><button type="button" class="btn btn-primary" id="bn_edit_save">Speichern</button>'
+    document.getElementById('edit_bn').innerHTML += newSpecTemplate.replace('%vehicle', vehicle)
+    document.getElementById('edit_bn').innerHTML += `<br><br><button type="button" class="btn btn-primary" id="bn_edit_save" onclick="saveVehicle('${vehicle}')">Speichern</button>`
     document.getElementById('edit_bn').innerHTML += '<br><button type="button" class="btn btn-secondary" id="bn_edit_back">Zurück</button>'
     document.getElementById("bn_edit_back").addEventListener("click", () => returnToMainWindow())
 }
 
-function updateSpecs(vehicle) {
-    let index = 0
-    const specs = httpGet(`http://${localStorage.getItem("ipv4")}/api/vehicles/${vehicle}/specs/${localStorage.getItem("sessionId")}`).res.split(";")
-    specs.forEach(spec => {
-        document.getElementById(`key_${index}`).value = spec.split("=")[0]
-        document.getElementById(`value_${index}`).value = spec.split("=")[1]
-        index++
-    })
-}
-
-function addSpec(vehicle) {
-    document.getElementById('edit_specs').innerHTML += specTemplate.replace(/%id/g, getIndex()).replace("%onclick", `deleteSpec(${getIndex()})`)
-    updateSpecs(vehicle)
-}
-
-function deleteSpec(index) {
-    document.remove(index)
-    let i = 0
-    document.getElementById("edit_specs").childNodes.forEach(node => {
-        node.childNodes.forEach(childNode => {
-            childNode.id = childNode.id.replace(node.id, i)
-            if(childNode.onclick) {
-                childNode.removeEventListener("click", () => deleteSpec(node.id))
-                childNode.addEventListener("click", () => deleteSpec(i))
-            }
-            /*if(childNode.id == `key_${node.id}`) childNode.id = `key_${i}`
-            if(childNode.id == `value_${node.id}`) childNode.id = `value_${i}`
-            if(childNode.id == `bn_delete_${node.id}`) childNode.id = `bn_delete_${i}`
-            if(childNode.onclick) childNode.onclick = () => deleteSpec(i)*/
-        })
-
-        node.id = i
-        i++
-    })
-}
-
-function getIndex() {
-    return document.getElementById('edit_specs').childNodes.length
+function saveVehicle(vehicle) {
+    var get = `http://${localStorage.getItem("ipv4")}/api/vehicles/${vehicle}/edit/${document.getElementById("manufacturer").value}/${document.getElementById("type").value}/${getSpecsFormatted()}/${localStorage.getItem("sessionId")}`
+    httpGet(get)
+    returnToMainWindow()
 }
 
 function returnToMainWindow() {
