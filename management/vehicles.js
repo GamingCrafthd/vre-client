cda5843809ff03f4c8cada23799ea5445d533020
JustMattis <60194722+JustMattis@users.noreply.github.com>
2021-08-09 12:56:57 +0200

    continue vehicle management

--- management/vehicles.js
+++ management/vehicles.js
@@ -1,7 +1,7 @@
 const select = document.getElementById("select")
 const vehicles = JSON.parse(httpGet(`http://${localStorage.getItem("ipv4")}/api/vehicles/${localStorage.getItem("sessionId")}`).res)
 const specTemplate = '<input id="key_%id"><input id="value_%id"><button class="btn btn-danger" id="bn_delete_%id">Löschen</button><br>'
-const newSpecTemplate = '<button type="button" class="btn btn-primary" id="bn_edit_new_spec_%spec" onclick="%onclick">Neue Spezifikation hinzufügen</button>'
+const newSpecTemplate = '<button type="button" class="btn btn-primary" id="bn_edit_new_spec_%vehicle" onclick="%onclick">Neue Spezifikation hinzufügen</button>'
 
 vehicles.forEach(vehicle => {
     var opt = document.createElement('option')
@@ -38,6 +38,9 @@ function openEdit(vehicle) {
         const key = spec.split("=")[0]
         const value = spec.split("=")[1]
         document.getElementById("edit_specs").innerHTML += specTemplate.replace(/%id/g, key)
+
+        document.getElementById(`key_${key}`).value = key
+        document.getElementById(`value_${key}`).value = value
     })
 
     const manufacturer = httpGet(`http://${localStorage.getItem("ipv4")}/api/vehicles/${vehicle}/manufacturer/${localStorage.getItem("sessionId")}`).res
@@ -48,16 +51,12 @@ function openEdit(vehicle) {
 
     document.getElementById('edit_bn').innerHTML = ""
 
-    document.getElementById('edit_bn').innerHTML += newSpecTemplate.replace('%spec', key).replace("%onclick", `openSpecEdit('${vehicle}', '')`)
+    document.getElementById('edit_bn').innerHTML += newSpecTemplate.replace('%vehicle', vehicle).replace("%onclick", `openSpecEdit('${vehicle}', '')`)
 
-    document.getElementById('edit_bn').innerHTML += '<br><button type="button" class="btn btn-primary" id="bn_edit_save">Speichern</button>'
+    document.getElementById('edit_bn').innerHTML += '<br><br><button type="button" class="btn btn-primary" id="bn_edit_save">Speichern</button>'
 
     document.getElementById('edit_bn').innerHTML += '<br><button type="button" class="btn btn-secondary" id="bn_edit_back">Zurück</button>'
     document.getElementById("bn_edit_back").addEventListener("click", () => returnToMainWindow())
-}
-
-function openSpecEdit(vehicle, spec) {
-    alert("open spec edit " + spec)
 }
 
 function returnToMainWindow() {
