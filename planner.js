463824bc163e9c9ef956cf229edd95e748d62889
Mattis <mattis@c-8.info>
2021-09-11 14:02:17 +0200

    continue planner

--- planner.js
+++ planner.js
@@ -1,4 +1,5 @@
-document.getElementById("planner_routeSelect").hidden = true
+const routeTemplate = '<div class="accordion-item" id="planner_routeSelect_accordition_%route"><h2 class="accordion-header" id="headingTwo"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">%route</button></h2><div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample"><div class="accordion-body">Start: %start<br>Ziel: %destination<br>Startzeit: %starttime<br>Endzeit: %endtime<br>Fahrzeuge: %vehicles<br>Wochentage: %daysofweek<br><br><select id="planner_routeSelect_accordition_%route_user"></select></div></div></div>'
+
 document.getElementById("planner_mapSelect_button").addEventListener("click", () => openMap(document.getElementById("planner_mapsSelect_select").value))
 
 function loadMaps() {
@@ -17,6 +18,7 @@ function openMap(map) {
     document.getElementById("planner_mapSelect").hidden = true
     document.getElementById("planner_routeSelect").hidden = false
 
+    document.getElementById("planner_routeSelect_accordition").innerHTML = ""
     document.getElementById("planner_routeSelect_name").innerHTML = `<h1>${map}</h1>`
     document.getElementById("planner_routeSelect_backButton").addEventListener("click", () => reset())
 }
