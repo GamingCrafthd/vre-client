51d14e81d70c8423d9a17cf7371929fb45aa69c3
Mattis <mattis@c-8.info>
2021-09-15 21:17:57 +0200

    continue planner

--- planner.js
+++ planner.js
@@ -1,4 +1,4 @@
-const routeTemplate = '<div class="accordion-item" id="planner_%section_accordition_%route"><h2 class="accordion-header" id="heading%section%index"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse%section%index" aria-expanded="false" aria-controls="collapse%section%index">%route</button></h2><div id="collapse%section%index" class="accordion-collapse collapse" aria-labelledby="heading%section%index" data-bs-parent="#accordionExample"><div class="accordion-body">Start: %firststop<br>Ziel: %destination<br>Startzeit: %starttime<br>Endzeit: %endtime<br>Fahrzeuge: %vehicles<br>Wochentage: %daysofweek<br><br><button class="btn btn-primary" type="button" id="planner_%section_accordition_%route_assign">Fahrer zuweisen</button><button class="btn btn-danger" type="button" id="planner_%section_accordition_%route_remove">Zuweisung löschen</button><div id="planner_%section_accordition_%route_misc"></div></div></div></div>'
+const routeTemplate = '<div class="accordion-item" id="planner_%section_accordition_%route"><h2 class="accordion-header" id="heading%section%index"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse%section%index" aria-expanded="false" aria-controls="collapse%section%index">%display</button></h2><div id="collapse%section%index" class="accordion-collapse collapse" aria-labelledby="heading%section%index" data-bs-parent="#accordionExample"><div class="accordion-body">Start: %firststop<br>Ziel: %destination<br>Startzeit: %starttime<br>Endzeit: %endtime<br>Fahrzeuge: %vehicles<br>Wochentage: %daysofweek<br><br><button class="btn btn-primary" type="button" id="planner_%section_accordition_%route_assign">Fahrer zuweisen</button><button class="btn btn-danger" type="button" id="planner_%section_accordition_%route_remove">Zuweisung löschen</button><div id="planner_%section_accordition_%route_misc"></div></div></div></div>'
 
 document.getElementById("planner_dayOfWeek").addEventListener("change", () => {
     if (document.getElementById("planner_mapSelect").hidden) {
@@ -64,9 +64,9 @@ function openMap(map) {
 
             const daysOfWeek = daysOfWeekRaw.toString().replace("1", "Mo ").replace("2", "Di ").replace("3", "Mi ").replace("4", "Do ").replace("5", "Fr ").replace("6", "Sa ").replace("7", "So ")
 
-            const assignee = api.map(`${map}/${index}/assignedUser`).res
+            const assignee = api.map(`${map}/${index}/${document.getElementById("planner_dayOfWeek").value}/assignedUser`).res
 
-            document.getElementById("planner_routeSelect_accordition").innerHTML += routeTemplate.replaceAll("%route", route).replaceAll("%firststop", firstStop).replaceAll("%destination", lastStop).replaceAll("%starttime", startTime).replaceAll("%endtime", endTime).replaceAll("%vehicles", vehicles).replaceAll("%daysofweek", daysOfWeek).replaceAll("%index", index).replaceAll("%section", "routeSelect")
+            document.getElementById("planner_routeSelect_accordition").innerHTML += routeTemplate.replaceAll("%display", route).replaceAll("%route", route).replaceAll("%firststop", firstStop).replaceAll("%destination", lastStop).replaceAll("%starttime", startTime).replaceAll("%endtime", endTime).replaceAll("%vehicles", vehicles).replaceAll("%daysofweek", daysOfWeek).replaceAll("%index", index).replaceAll("%section", "routeSelect")
 
             document.getElementById(`planner_routeSelect_accordition_${route}_remove`).hidden = true
 
@@ -111,7 +111,7 @@ function openUser(user) {
 
             const dayOfWeek = dayOfWeekRaw.toString().replace("1", "Mo ").replace("2", "Di ").replace("3", "Mi ").replace("4", "Do ").replace("5", "Fr ").replace("6", "Sa ").replace("7", "So ")
 
-            document.getElementById("planner_userRoutes_accordition").innerHTML += routeTemplate.replaceAll("%route", route).replaceAll("%firststop", firstStop).replaceAll("%destination", lastStop).replaceAll("%starttime", startTime).replaceAll("%endtime", endTime).replaceAll("%vehicles", vehicles).replaceAll("%daysofweek", dayOfWeek).replaceAll("%index", index).replaceAll("%section", "userRoutes")
+            document.getElementById("planner_userRoutes_accordition").innerHTML += routeTemplate.replaceAll("%display", `${map}: ${route}`).replaceAll("%route", `${route}`).replaceAll("%firststop", firstStop).replaceAll("%destination", lastStop).replaceAll("%starttime", startTime).replaceAll("%endtime", endTime).replaceAll("%vehicles", vehicles).replaceAll("%daysofweek", dayOfWeek).replaceAll("%index", index).replaceAll("%section", "userRoutes")
 
             document.getElementById(`planner_userRoutes_accordition_${route}_assign`).hidden = true
             document.getElementById(`planner_userRoutes_accordition_${route}_remove`).addEventListener("click", () => {
@@ -125,7 +125,9 @@ function openUser(user) {
 
 function deleteRoute(route, user) {
     let routes = api.user(`${user}/routes`).res
-    routes = routes.replace(`${route}${routes.includes(route + ",") ? "," : ""}`, "")
+    routes.replace(route, "").replace(",,", ",")
+    if (routes.endsWith(",")) routes = routes.slice(0, routes.length - 1)
+    if (routes.startsWith(",")) routes = routes.slice(1)
     api.user(`${document.getElementById("planner_userRoutes_name").innerHTML.replace("<h1>", "").replace("</h1>", "")}/set/routes/${routes === "" ? "NONE" : routes}`)
 }
 
